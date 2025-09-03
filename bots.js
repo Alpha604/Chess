7738591c81eab68ba87a626f2a735c4f8682814d
Roméo Leonard <romeo.leonard56@gmail.com>
2025-09-03 18:51:40 +0200

    Add files via upload

---
+++ bots.js
@@ -0,0 +1,20 @@
+// Exemple de bots avec leur pays
+const bots = {
+    evenemment: [
+
+    ],
+    debutant: [
+        {name: "Martin", elo: 250, img:"https://images.chesscomfiles.com/uploads/v1/bot_personality/a6e19caa-8a5a-11ea-b74e-55003725fb61.6cfa3204.384x384o.6ed7394700f6.png", country:"BG", locked: false},
+        {name: "Elani", elo: 400, img:"https://images.chesscomfiles.com/uploads/v1/bot_personality/c62b39f4-8a5a-11ea-bbe4-51aff3d2d9d5.c7d554bf.384x384o.d253af8b28cc.png", country:"GR", locked: false},
+        {name: "Janjay", elo: 700, img:"https://images.chesscomfiles.com/uploads/v1/bot_personality/79d79290-8a5b-11ea-a7cb-a9719a339cd3.82a163ad.384x384o.b1ac54f73ac9.png", country:"LR", locked: true},
+    ],
+    avance: [
+        {name: "Wendy", elo: 1500, img:"https://images.chesscomfiles.com/uploads/v1/bot_personality/30326f88-8a5c-11ea-b234-9b639e301bef.adf1facd.384x384o.39ecdf8795d5.png", country:"CA", locked: true, url:"Setings/locked.html"},
+    ],
+    maitre: [
+
+    ],
+    adaptatif: [
+    
+    ]
+};
